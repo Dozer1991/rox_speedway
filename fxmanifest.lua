d662cd8302609886f2dfaf4bb2eec1904c13df44
DankScripts <docr.greene@gmail.com>
2025-05-22 01:00:14 -0500

    update file structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,35 +1,38 @@
 fx_version 'cerulean'
 game 'gta5'
 
+lua54 'yes'
+
 author 'DrCannabis'
 description '(Original Author Koala) Alternate version of max_rox_speedway edited by DrCannabis'
 
 shared_scripts {
-	'@ox_lib/init.lua',
-    '@qb-core/shared/locale.lua',
+  '@ox_lib/init.lua',
+  'config/config.lua',
+  'locales/*.lua',           -- load your Lua locale modules
 }
 
 client_scripts {
-    'client/*.lua',
+  'client/c_fuel.lua',       -- <-- matches your filename
+  'client/c_function.lua',
+  'client/c_main.lua',
+  'client/c_pit.lua',
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server/s_main.lua'
-   
+  '@oxmysql/lib/MySQL.lua',
+  'server/s_main.lua',
+  'server/s_leaderboard.lua',
 }
 
 files {
-    'config/config.lua',
-    'locales/*.lua'
+    'locales/*.lua',
 }
 
 dependencies {
     'ox_lib',
     'qb-core',
     'qb-target',
+    'qb-input',
     'oxmysql',
 }
-
-lua54 'yes'
-use_experimental_fxv2_oal 'yes'
