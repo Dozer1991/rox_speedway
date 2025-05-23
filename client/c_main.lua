5cf87ed656b45e7db8cec0fc2f9fa6af26af4f28
DankScripts <docr.greene@gmail.com>
2025-05-23 16:24:31 -0500

    vehicles no longer drive before countdown

--- client/c_main.lua
+++ client/c_main.lua
@@ -350,8 +350,8 @@ RegisterNetEvent("speedway:prepareStart", function(data)
         while not DoesEntityExist(veh) do Wait(0); veh = NetworkGetEntityFromNetworkId(data.netId) end
 
         SetEntityAsMissionEntity(veh, true, true)
-        FreezeEntityPosition(veh, true)
         TaskWarpPedIntoVehicle(PlayerPedId(), veh, -1)
+        FreezeEntityPosition(veh, true)
         repeat Wait(0) until IsPedInAnyVehicle(PlayerPedId(), false)
 
         for i = 3, 1, -1 do ShowCountdownText(tostring(i), 1000) end
